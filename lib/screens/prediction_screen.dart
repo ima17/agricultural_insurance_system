ecb3628e7d269c20dc4136d9f3649708672568c3
ima17 <ima.sudaraka@outlook.com>
2023-06-14 23:19:39 +0530

    Add prediction screen

---
+++ lib/screens/prediction_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PredictionScreen extends StatefulWidget {
+  const PredictionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PredictionScreen> createState() => _PredictionScreenState();
+}
+
+class _PredictionScreenState extends State<PredictionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Prediction Screen");
+  }
+}
