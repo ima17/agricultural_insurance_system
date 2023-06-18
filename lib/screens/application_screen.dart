795c88720e30382535452d59cb0bc8c9c99e7b0e
ima17 <ima.sudaraka@outlook.com>
2023-06-19 03:46:20 +0530

    Update application screen

--- lib/screens/application_screen.dart
+++ lib/screens/application_screen.dart
@@ -1,9 +1,19 @@
 import 'package:agricultural_insurance_system/models/application_data.dart';
+import 'package:agricultural_insurance_system/screens/map_screen.dart';
 import 'package:flutter/material.dart';
 
-class ApplicationScreen extends StatelessWidget {
+class ApplicationScreen extends StatefulWidget {
   final ApplicationData? applicationData;
-  const ApplicationScreen({super.key, required this.applicationData});
+
+  const ApplicationScreen({Key? key, required this.applicationData})
+      : super(key: key);
+
+  @override
+  _ApplicationScreenState createState() => _ApplicationScreenState();
+}
+
+class _ApplicationScreenState extends State<ApplicationScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,45 +24,118 @@ class ApplicationScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          key: _formKey,
+          child: ListView(
             children: [
               TextFormField(
-                initialValue: applicationData!.name,
+                initialValue: widget.applicationData!.name,
                 decoration: const InputDecoration(labelText: 'නම'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'නම ඇතුළත් කරන්න';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  widget.applicationData!.name = value!;
+                },
               ),
               TextFormField(
-                initialValue: applicationData!.address,
+                initialValue: widget.applicationData!.address,
                 decoration: const InputDecoration(labelText: 'ලිපිනය'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'ලිපිනය ඇතුළත් කරන්න';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  widget.applicationData!.address = value!;
+                },
               ),
               TextFormField(
-                initialValue: applicationData!.contactNumber,
-                decoration: const InputDecoration(labelText: 'හැඳුනුම්පත් අංකය'),
+                initialValue: widget.applicationData!.contactNumber,
+                decoration:
+                    const InputDecoration(labelText: 'හැඳුනුම්පත් අංකය'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'හැඳුනුම්පත් අංකය ඇතුළත් කරන්න';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  widget.applicationData!.contactNumber = value!;
+                },
               ),
               TextFormField(
-                initialValue: applicationData!.dateOfBirth,
+                initialValue: widget.applicationData!.dateOfBirth,
                 decoration: const InputDecoration(labelText: 'උපන්දිනය'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'උපන්දිනය ඇතුළත් කරන්න';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  widget.applicationData!.dateOfBirth = value!;
+                },
               ),
               TextFormField(
-                initialValue: applicationData!.gender,
+                initialValue: widget.applicationData!.gender,
                 decoration: const InputDecoration(labelText: 'වී වර්ගය'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'වී වර්ගය ඇතුළත් කරන්න';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  widget.applicationData!.gender = value!;
+                },
               ),
               TextFormField(
-                initialValue: applicationData!.occupation,
+                initialValue: widget.applicationData!.occupation,
                 decoration: const InputDecoration(labelText: 'කන්නය'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'කන්නය ඇතුළත් කරන්න';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  widget.applicationData!.occupation = value!;
+                },
               ),
               TextFormField(
-                initialValue: applicationData!.age,
+                initialValue: widget.applicationData!.age,
                 decoration: const InputDecoration(labelText: 'වගා ක්‍රමය'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'වගා ක්‍රමය ඇතුළත් කරන්න';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  widget.applicationData!.age = value!;
+                },
               ),
               TextFormField(
-                initialValue: applicationData!.propertySize,
+                initialValue: widget.applicationData!.propertySize,
                 decoration: const InputDecoration(labelText: 'ඉඩමේ ප්‍රමාණය'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'ඉඩමේ ප්‍රමාණය ඇතුළත් කරන්න';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  widget.applicationData!.propertySize = value!;
+                },
               ),
               const SizedBox(height: 16.0),
               ElevatedButton(
                 onPressed: () {
-                  // Handle form submission
+                  _submitForm();
                 },
                 child: const Text('Submit'),
               ),
@@ -61,5 +144,21 @@ class ApplicationScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      // Process the form submission
+      print(widget.applicationData.toString());
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MapScreen(
+                  applicationData: widget.applicationData,
+                )),
+      );
+    }
   }
 }
