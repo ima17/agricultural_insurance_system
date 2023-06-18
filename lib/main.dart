20c5efdd07f9782307535960ded98ee8b719c473
ima17 <ima.sudaraka@outlook.com>
2023-06-19 04:43:54 +0530

    Update theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:agricultural_insurance_system/screens/recording_screen.dart';
 import 'package:flutter/material.dart';
 
-
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: RecordingScreen(),
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green, // Set the primary color to green
+      ),
+      home: const RecordingScreen(),
+      debugShowCheckedModeBanner: false, // Hide the debug banner
     );
   }
 }
