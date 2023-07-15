a622bbe57037e864ca03fddf259a00d4606ed693
imantha <ima.sudaraka@outlook.com>
2023-07-15 21:15:12 +0530

    Update color palette

---
+++ lib/configs/palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  // Common Colors
+  static const kprimaryColor = Color(0xFF4abf78);
+  static const kHeaderTextColor = Color(0xFF000000);
+  static const kSubtitleTextColor = Color(0xFF515C6F);
+  static const kWhiteColor = Color(0xFFFFFFFF);
+  static const kdropShadowColor = Color(0xFFE2E5EC);
+  static const kdarkBlackColor = Color(0xFF000000);
+  static const klightWhiteColor = Color(0xFFFFFFFF);
+  static const kheadingColor = Color(0xFF515C6F);
+  static const kbackgroundColor = Color(0xFFF0F2F6);
+}
