e42a59554c6fccebef588f3ff7fd816f1933a519
imantha <ima.sudaraka@outlook.com>
2023-08-10 00:41:45 +0530

    Refactor and UI changes

--- lib/screens/risks_show_screen.dart
+++ lib/screens/risks_show_screen.dart
@@ -1,61 +1,19 @@
-import 'package:agricultural_insurance_system/constants/url_links.dart';
+import 'package:agricultural_insurance_system/widgets/location_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:agricultural_insurance_system/models/application_data.dart';
 import 'package:agricultural_insurance_system/models/district_data.dart';
 import 'package:agricultural_insurance_system/models/flood_risk_data.dart';
 import 'package:agricultural_insurance_system/models/whether_risk_data.dart';
 import 'package:agricultural_insurance_system/screens/prediction_screen.dart';
-import 'package:agricultural_insurance_system/screens/risks_show_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:agricultural_insurance_system/widgets/risk_card.dart';
+import 'package:agricultural_insurance_system/constants/url_links.dart';
+import 'package:agricultural_insurance_system/services/risk_calculate_service.dart';
 
-import 'package:charts_flutter/flutter.dart' as charts;
-
-import 'risks_show_screen.dart';
-
-class ProgressIndicatorWidget extends StatelessWidget {
-  final String value;
-
-  ProgressIndicatorWidget({required this.value});
-
-  @override
-  Widget build(BuildContext context) {
-    Color progressColor;
-    double progressValue;
-
-    switch (value) {
-      case 'No':
-        progressColor = Colors.green;
-        progressValue = 0.2;
-        break;
-      case 'Moderate':
-        progressColor = Color.fromARGB(255, 212, 171, 36);
-        progressValue = 0.55;
-        break;
-      case 'High':
-        progressColor = Colors.red;
-        progressValue = 0.95;
-        break;
-      default:
-        progressColor = Colors.transparent;
-        progressValue = 0.0;
-    }
-
-    return SizedBox(
-      height: 30, // Set the desired height
-      child: ClipRRect(
-        borderRadius:
-            BorderRadius.circular(20), // Set the desired border radius
-        child: LinearProgressIndicator(
-          value: progressValue,
-          backgroundColor: Colors.grey[300],
-          valueColor: AlwaysStoppedAnimation<Color>(progressColor),
-        ),
-      ),
-    );
-  }
-}
+import '../configs/palette.dart';
+import '../widgets/button_widget.dart';
 
 class RiskShowScreen extends StatefulWidget {
   final ApplicationData? applicationData;
@@ -72,92 +30,32 @@ class RiskShowScreen extends StatefulWidget {
 }
 
 class _RiskShowScreenState extends State<RiskShowScreen> {
+  RiskCalculateService riskCalculateService = RiskCalculateService();
   DistrictData? districtData;
   FloodRiskData? floodRiskData;
   WeatherRiskData? weatherRiskData;
   bool isLoading = true;
-
   @override
   void initState() {
     super.initState();
-    _getData();
-  }
-
-  Future<void> _getData() async {
-    print(
-        'Latitude: ${widget.currentPosition.latitude}, Longitude: ${widget.currentPosition.latitude}');
-
-    final headers = {'Content-Type': 'application/json'};
-    final requestBody1 = {
-      'lat': widget.currentPosition.latitude,
-      'lon': widget.currentPosition.longitude,
-    };
-    final requestBody2 = {
-      'lat': widget.currentPosition.latitude,
-      'lon': widget.currentPosition.longitude,
-      'season': widget.applicationData!.occupation,
-    };
-
-    try {
-      final response1 = await http.post(Uri.parse(admBoundryLink),
-          headers: headers, body: jsonEncode(requestBody1));
-
-      if (response1.statusCode == 200) {
-        // Request 1 successful, handle the response here
-        print('API 1 response: ${response1.body}');
-        districtData = DistrictData.fromJson(jsonDecode(response1.body));
-
-        final response2 = await http.post(Uri.parse(floodRiskLink),
-            headers: headers, body: jsonEncode(requestBody2));
-
-        if (response2.statusCode == 200) {
-          // Request 2 successful, handle the response here
-          print('API 2 response: ${response2.body}');
-          floodRiskData = FloodRiskData.fromJson(jsonDecode(response2.body));
-
-          // Extract the necessary value from response 1 and assign it to request body 3
-          final valueFromResponse1 = districtData?.district;
-          final requestBody3 = {
-            'location': valueFromResponse1,
-            'year': 2011,
-            'season': widget.applicationData!.occupation
-          };
-
-          final response3 = await http.post(Uri.parse(weatherRiskLink),
-              headers: headers, body: jsonEncode(requestBody3));
-
-          if (response3.statusCode == 200) {
-            // Request 3 successful, handle the response here
-            print('API 3 response: ${response3.body}');
-            weatherRiskData =
-                WeatherRiskData.fromJson(jsonDecode(response3.body));
-          } else {
-            // Request 3 failed, handle the error
-            print(
-                'API request 3 failed with status code ${response3.statusCode}');
-          }
-        } else {
-          // Request 2 failed, handle the error
-          print(
-              'API request 2 failed with status code ${response2.statusCode}');
-        }
-      } else {
-        // Request 1 failed, handle the error
-        print('API request 1 failed with status code ${response1.statusCode}');
-      }
-    } catch (error) {
-      // An error occurred while making the request
-      print('Error making API request: $error');
-    }
-
-    setState(() {
-      isLoading = false; // Set isLoading to false after receiving the data
+    riskCalculateService
+        .getData(
+      widget.currentPosition.latitude,
+      widget.currentPosition.longitude,
+      widget.applicationData!.occupation,
+    )
+        .then((_) {
+      setState(() {
+        isLoading = false;
+        districtData = riskCalculateService.districtData;
+        floodRiskData = riskCalculateService.floodRiskData;
+        weatherRiskData = riskCalculateService.weatherRiskData;
+      });
     });
   }
 
-  get floodRisk => floodRiskData!.floodRisk;
-  get weatherRisk => weatherRiskData!.weatherRisk;
-
+  get floodRisk => floodRiskData?.floodRisk;
+  get weatherRisk => weatherRiskData?.weatherRisk;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -166,10 +64,13 @@ class _RiskShowScreenState extends State<RiskShowScreen> {
       ),
       body: Center(
         child: isLoading
-            ? const Column(
+            ? Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  CircularProgressIndicator(),
+                  SpinKitDoubleBounce(
+                    color: Palette.kPrimaryColor,
+                    size: 100.0,
+                  ),
                   SizedBox(height: 16),
                   Text(
                     'Calculating Risks',
@@ -182,152 +83,76 @@ class _RiskShowScreenState extends State<RiskShowScreen> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    // First Section: Location Details
-                    Container(
-                      color: Colors.grey[200],
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Location Details',
-                            style: TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 16),
-                          if (districtData != null)
-                            Table(
-                              border: TableBorder.all(
-                                  color: Colors.black, width: 1.0),
-                              columnWidths: const {
-                                0: FlexColumnWidth(),
-                                1: FlexColumnWidth(),
-                              },
-                              children: [
-                                TableRow(
-                                  decoration: BoxDecoration(
-                                      color: const Color.fromARGB(
-                                          255, 255, 255, 255)),
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        'District',
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        'GN Division',
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                TableRow(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        districtData!.district,
-                                        style: TextStyle(fontSize: 18),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        districtData!.gnd,
-                                        style: TextStyle(fontSize: 18),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                        ],
-                      ),
+                    Text(
+                      'Location Details',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(height: 36),
-                    Divider(), // Add divider between sections
-                    SizedBox(height: 36),
-                    // Second Section: Risk Levels
+                    SizedBox(height: 16),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: LocationCard(
+                              title: 'District',
+                              subTitle: districtData!.district,
+                              icon: FontAwesomeIcons.city),
+                        ),
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: LocationCard(
+                            title: 'GND',
+                            subTitle: districtData!.gnd,
+                            icon: FontAwesomeIcons.locationPin,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 18),
+                    Divider(
+                      color: Palette.kPrimaryColor,
+                    ),
+                    SizedBox(height: 18),
+                    Text(
+                      'Risks Details',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 16),
+                    RiskCard(
+                      title: "Flood Risk",
+                      icon: FontAwesomeIcons.houseFloodWater,
+                      riskLevel: 'High',
+                    ),
+                    SizedBox(height: 10),
+                    RiskCard(
+                      title: "Weather Risk",
+                      icon: FontAwesomeIcons.cloudShowersWater,
+                      riskLevel: 'No',
+                    ),
+                    Spacer(),
                     Container(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Risk Levels',
-                            style: TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 16),
-                          //progress bar
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 16.0),
-                            child: Text(
-                              'Flood Risk: $floodRisk',
-                              style: TextStyle(fontSize: 18),
-                              textAlign: TextAlign.center,
+                      // padding: const EdgeInsets.all(16.0),
+                      child: ButtonWidget(
+                        buttonText: 'Save and Next',
+                        buttonTriggerFunction: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PredictionScreen(
+                                applicationData: widget.applicationData,
+                                districtData: districtData,
+                                weatherRiskData: weatherRiskData,
+                                floodRiskData: floodRiskData,
+                              ),
                             ),
-                          ),
-                          ProgressIndicatorWidget(value: floodRisk),
-                          SizedBox(height: 42),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 16.0),
-                            child: Text(
-                              'Weather Risk: $weatherRisk',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          ),
-                          ProgressIndicatorWidget(value: weatherRisk),
-                        ],
+                          );
+                        },
                       ),
                     ),
                   ],
                 ),
               ),
       ),
-      floatingActionButton: ElevatedButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => PredictionScreen(
-                applicationData: widget.applicationData,
-                districtData: districtData,
-                weatherRiskData: weatherRiskData,
-                floodRiskData: floodRiskData,
-              ),
-            ),
-          );
-        },
-        child: const Text(
-          'Save and Next',
-          style: TextStyle(fontSize: 18),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
-    
-    
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
